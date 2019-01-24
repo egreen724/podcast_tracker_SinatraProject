614af3864897a5925f03c53bf997fe231293723d
Erin Green <erin.y.green@gmail.com>
2019-01-24 02:31:48 +0000

    adds post signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
     end
   end
 
+  post 'signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup' #add a flash message
+    else
+      @user = User.new(params[:user])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/podcasts'
+    end 
+  end
+
+
   get '/login' do
     if logged_in?
       redirect to '/podcasts'
