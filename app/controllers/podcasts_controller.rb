29ffbde64aafc1eb320a9031a5fbd51ba979ba9a
Erin Green <erin.y.green@gmail.com>
2019-01-24 02:10:41 +0000

    adds podcasts routes

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,3 +1,8 @@
 class PodcastsController < ApplicationController
-  
+
+
+  get "/podcasts" do
+    erb :"/podcasts/all_podcasts"
+
+  end
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,4 +1,9 @@
 class Podcast < ActiveRecord::Base
   belongs_to :user
-  
+
+  get "/podcasts" do
+    erb :all_podcasts
+    
+  end
+
 end
