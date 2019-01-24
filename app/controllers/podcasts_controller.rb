02ebf7d411358141878aca16182a048c95a4cd54
Erin Green <erin.y.green@gmail.com>
2019-01-24 02:58:53 +0000

    adds create podcast form and route

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -11,4 +11,20 @@ class PodcastsController < ApplicationController
     end
   end
 
+  get "/podcasts/new" do
+    erb :'/podcasts/new_podcast'
+  end
+
+  post "/podcasts" do
+    @user = User.find_by_id(session[:user_id])
+
+    if logged_in?
+      @podcast = Podcast.new(:title => params[:title], :description => params[:description])
+      @user.podcasts << @podcast
+      @user.save
+    else
+      redirect to '/login'
+    end
+  end
+
 end
