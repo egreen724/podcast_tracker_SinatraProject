e7d6b789dc49f9f9ef119cb7d720cd0b33347796
Erin Green <erin.y.green@gmail.com>
2019-01-24 21:36:11 +0000

    Adds controls for incorrect email addresses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,13 @@ use Rack::Flash
   end
 
   post '/signup' do
-
+    binding.pry
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-       flash[:message] = "Please fill in the appropriate fields to create an account."
+      flash[:message] = "Please fill in the appropriate fields to create an account."
       redirect to '/signup' #add a flash message
+    elsif !params[:email].include?("@")
+      flash[:message] = "Please fill in the appropriate fields to create an account."
+      redirect to '/signup'
     else
       @user = User.create(params[:user])
       session[:user_id] = @user.id
