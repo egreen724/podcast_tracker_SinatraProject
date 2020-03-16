a9500bbf41ad1fca57cc54d8ae9cd8dcf4b6893c
github username <github email address>
2020-03-16 16:38:14 -0400

    fixes environment file

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
-gem 'activerecord', '4.2.5', :require => 'active_record'
+gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
@@ -13,8 +13,9 @@ gem "tux"
 gem 'sinatra-flash'
 
 
+
 group :development do
-  gem 'sqlite3'
+   gem 'sqlite3'
 end
 
 group :production do
--- config/environment.rb
+++ config/environment.rb
@@ -1,44 +1,27 @@
+
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
 configure :development do
   ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-end
-
-configure :production do
-  db = URI.parse(ENV['postgres://xzdwajwhfcskel:f7e5e8fb537a255ca75a790e04fe43575f072c2271fd6ca7d2fad72745796182@ec2-184-72-235-159.compute-1.amazonaws.com:5432/db3gevq2q6cg5e'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
 end
 
-# ENV['SINATRA_ENV'] ||= "development"
-#
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-#
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-#
-# configure :production do
-#    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-#
-#    ActiveRecord::Base.establish_connection(
-#      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#      :host     => db.host,
-#      :username => db.user,
-#      :password => db.password,
-#      :database => db.path[1..-1],
-#      :encoding => 'utf8'
-#      )
-# end
-#
-# require_all 'app'
+configure :production do
+   db = URI.parse(ENV['postgres://xzdwajwhfcskel:f7e5e8fb537a255ca75a790e04fe43575f072c2271fd6ca7d2fad72745796182@ec2-184-72-235-159.compute-1.amazonaws.com:5432/db3gevq2q6cg5e'] || 'postgres://localhost/mydb')
+
+   ActiveRecord::Base.establish_connection(
+     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host     => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
+     )
+end
+
+require_all 'app'
